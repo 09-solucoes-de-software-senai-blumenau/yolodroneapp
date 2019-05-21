3e1f14f3ba60c4df45f1c313d587ba4cb9ed3b87
Fagundes <thiago_custodio@estudante.sc.senai.br>
2019-05-21 15:30:09 -0300

    21/05/2019

---
+++ testealturosyolo/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace testealturosyolo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.dataGridView2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(134, 582);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(143, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(464, 263);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Red;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(73, 56);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(26, 33);
+            this.panel2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(40, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "cor";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "n° pessoas: null";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "media %: NaN";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(143, 280);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(464, 290);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(11, 215);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(107, 166);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "cameras";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 387);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "resoluções";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(11, 403);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(107, 166);
+            this.dataGridView2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(619, 582);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ testealturosyolo/Form1.cs
@@ -0,0 +1,161 @@
+﻿using Alturos.Yolo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DarrenLee.Media;
+
+namespace testealturosyolo
+{
+    public partial class Form1 : Form
+    {
+        Color corquadrado = Color.Red;
+        Camera c;
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                c = new Camera();
+                getinfo();
+                c.OnFrameArrived += C_OnFrameArrived;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("nenhuma camera disponivel!");
+            }
+            
+        }
+
+        private void getinfo()
+        {
+            var cdv = c.GetCameraSources();
+            var cresolu = c.GetSupportedResolutions();
+            dataGridView1.DataSource = cdv.ToList();
+            dataGridView2.DataSource = cresolu.ToList();
+        }
+
+        private void C_OnFrameArrived(object source, FrameArrivedEventArgs e)
+        {
+            pictureBox2.Image = e.GetFrame();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog of = new OpenFileDialog();
+            of.Filter = "png|*.png|jpg|*.jpg";
+            double total = 0;
+            if (of.ShowDialog() == DialogResult.OK)
+            {
+                var ms = new MemoryStream();
+                Bitmap b = new Bitmap(of.FileName);
+                b.Save(ms, ImageFormat.Jpeg);
+                
+               
+                var configurationDetector = new ConfigurationDetector();
+                var config = configurationDetector.Detect();
+                
+                using (var yoloWrapper = new YoloWrapper(config))
+                {
+                    var items = yoloWrapper.Detect(ms.ToArray());
+                    //items[0].Type -> "Person, Car, ..."
+                    //items[0].Confidence -> 0.0 (low) -> 1.0 (high)
+                    //items[0].X -> bounding box
+                    //items[0].Y -> bounding box
+                    //items[0].Width -> bounding box
+                    //items[0].Height -> bounding box
+                    Pen p = new Pen(Brushes.Red, 20);
+                    label1.Text = $"n° pessoas: {items.ToList().Count()}";
+                    foreach (var item in items)
+                    {
+                        total += item.Confidence;
+                        if (item.Type.ToLower() == "person")
+                        {
+                            //desenha quadrado
+
+                            for (int i2 = 0; i2 < 5; i2++)
+                            {
+                                for (int i = 0; i < item.Width; i++)
+                                {
+                                    try
+                                    {
+                                        b.SetPixel(item.X + i, item.Y + i2, corquadrado);
+                                    }
+                                    catch (Exception)
+                                    {}
+                                }
+                            }
+                            for (int i2 = 0; i2 < 5; i2++)
+                            {
+                                for (int i = 0; i < item.Width+5; i++)
+                                {
+                                    try
+                                    {
+                                        b.SetPixel(item.X + i, item.Y + i2 + item.Height, corquadrado);
+                                    }
+                                    catch (Exception)
+                                    {}
+                                }
+                            }
+                            for (int i2 = 0; i2 < 5; i2++)
+                            {
+                                for (int i = 0; i < item.Height; i++)
+                                {
+                                    try
+                                    {
+                                        b.SetPixel(item.X + i2, item.Y + i, corquadrado);
+                                    }
+                                    catch (Exception)
+                                    { }
+                                }
+                            }
+                            for (int i2 = 0; i2 < 5; i2++)
+                            {
+                                for (int i = 0; i < item.Height; i++)
+                                {
+                                    try
+                                    {
+                                        b.SetPixel(item.X + i2 + item.Width, item.Y + i, corquadrado);
+                                    }
+                                    catch (Exception)
+                                    {}
+                                }
+                            }
+
+                        }
+
+                    }
+                    double a = 0;
+                    a = total / Convert.ToDouble(items.ToList().Count());
+                    a = Math.Round(a, 4);
+                    a = a * 100;
+                    label2.Text = $"media %: {a}";
+                    pictureBox1.Image = b;
+                }
+                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ColorDialog c = new ColorDialog();
+            if(c.ShowDialog() == DialogResult.OK)
+            {
+                corquadrado = c.Color;
+                panel2.BackColor = c.Color;
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
